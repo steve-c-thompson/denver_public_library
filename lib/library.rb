c06a00b44d21b4cb836f3aa2623e660676f5161e
Steve Thompson <scthomps312@gmail.com>
2018-06-25 21:26:32 -0600

    Iteration 4, can now see if a title is in the library, and get card catalogue sorted by two fields

--- lib/library.rb
+++ lib/library.rb
@@ -7,4 +7,12 @@ class Library
   def add_to_collection(book)
     @books.push(book)
   end
+
+  def include?(title)
+    @books.any? {|book| book.title == title}
+  end
+
+  def card_catalog
+    books.sort_by {|b| [b.author_last_name, b.title]}
+  end
 end
--- test/library_test.rb
+++ test/library_test.rb
@@ -32,4 +32,43 @@ class LibraryTest < Minitest::Test
     assert_equal "Villette", dpl.books[2].title
   end
 
+  def test_it_returns_true_for_titles_in_books_collection
+    dpl = Library.new
+    charlotte_bronte = Author.new({first_name: "Charlotte", last_name: "Bronte"})
+    jane_eyre = charlotte_bronte.add_book("Jane Eyre", "October 16, 1847")
+    villette  = charlotte_bronte.add_book("Villette", "1853")
+    dpl.add_to_collection(jane_eyre)
+
+    harper_lee  = Author.new({first_name: "Harper", last_name: "Lee"})
+    mockingbird = harper_lee.add_book("To Kill a Mockingbird", "July 11, 1960")
+
+    dpl.add_to_collection(mockingbird)
+    dpl.add_to_collection(villette)
+
+    assert dpl.include?("To Kill a Mockingbird")
+    refute dpl.include?("A Connecticut Viking in King Arthur's Court")
+  end
+
+  def test_card_catalog_returns_array_of_books_in_alphabetical_order_sorted_by_author_last_name
+    dpl = Library.new
+    charlotte_bronte = Author.new({first_name: "Charlotte", last_name: "Bronte"})
+    jane_eyre = charlotte_bronte.add_book("Jane Eyre", "October 16, 1847")
+    villette  = charlotte_bronte.add_book("Villette", "1853")
+    dpl.add_to_collection(jane_eyre)
+
+    harper_lee  = Author.new({first_name: "Harper", last_name: "Lee"})
+    mockingbird = harper_lee.add_book("To Kill a Mockingbird", "July 11, 1960")
+
+    dpl.add_to_collection(mockingbird)
+    dpl.add_to_collection(villette)
+
+    books = dpl.card_catalog
+    assert_equal 3, books.count
+    assert_equal "Bronte", books[0].author_last_name
+    assert_equal "Jane Eyre", books[0].title
+    assert_equal "Bronte", books[1].author_last_name
+    assert_equal "Villette", books[1].title
+    assert_equal "Lee", books[2].author_last_name
+  end
+
 end
